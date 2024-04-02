a0cd43831cecf90ef6ce7fc54d8d273a487166bb
Chris DiStasio <stahzz1970@gmail.com>
2024-04-02 18:16:00 -0500

    Completes

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def index():
+  return "Hello, this PetFax!"
+
+@app.route('/pets')
+
+def pets():
+  return "These are our pets available for adoption!"
